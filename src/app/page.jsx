883a4c466331235e1c3369e04cf8dfd134bdea14
Rajkumar Shah <myrajkumar2003@gmail.com>
2026-03-10 23:09:46 +0530

    01 Project Setup

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> test</button>
+   </div>
+  );
+}
